41ac6139c826951f855cfba1e0240115fb1e2f15
Mark Howells-Mead <mark@sayhello.ch>
2026-03-06 12:34:20 +0100

    cache busting

--- list.php
+++ list.php
@@ -1,3 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+function cacheBustedAsset(string $relativePath): string
+{
+    $absolutePath = __DIR__ . '/' . ltrim($relativePath, '/');
+    $version = is_file($absolutePath) ? (string) filemtime($absolutePath) : (string) time();
+
+    return $relativePath . '?v=' . $version;
+}
+
+$listCssUrl = cacheBustedAsset('list.css');
+$appJsUrl = cacheBustedAsset('app.js');
+?>
 <!doctype html>
 <html lang="en">
 
@@ -5,7 +20,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Gallery</title>
-    <link rel="stylesheet" href="list.css">
+    <link rel="stylesheet" href="<?php echo htmlspecialchars($listCssUrl, ENT_QUOTES, 'UTF-8'); ?>">
 </head>
 
 <body>
@@ -13,7 +28,7 @@
     <div id="status">Loading…</div>
     <ul id="image-list"></ul>
 
-    <script src="app.js"></script>
+    <script src="<?php echo htmlspecialchars($appJsUrl, ENT_QUOTES, 'UTF-8'); ?>"></script>
 </body>
 
 </html>
